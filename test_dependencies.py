0844c87cdd1d8fc3f9bdde1f8f0e250dcabbf202
InanisV <zhanzhengdao@gmail.com>
2020-06-10 09:59:46 +0800

    update dependency test

--- test_dependencies.py
+++ test_dependencies.py
@@ -1,3 +1,4 @@
+import re
 import os
 import sys
 import math
@@ -6,8 +7,10 @@ import queue
 import pickle
 import threading
 from peewee import *
+from tqdm import tqdm
 import PySimpleGUI as sg
 from Txporn import Txporn
 from textblob import TextBlob
 from collections import Counter
+from nltk.tokenize import RegexpTokenizer
 from asyncio import set_event_loop, new_event_loop, get_event_loop, wait, ensure_future
